5436782f660ba54066bfdec94ef4c6ade4e64f18
RafPe <rafal@pieniazek.nl>
2020-10-21 17:03:04 +0200

    addin traefik manifest

--- src/github.com/Lepkem/traefik-plugin-response-code-override/rcoverride.go
+++ src/github.com/Lepkem/traefik-plugin-response-code-override/rcoverride.go
@@ -1,30 +1,90 @@
-package traefik-plugin-response-code-override
+package traefik_plugin_response_code_override
+
+import (
+	"bytes"
+	"context"
+	"net/http"
+)
 
 // Config the plugin configuration.
 type Config struct {
+	Overrides  map[int]int `json:"overrides,omitempty"`
+	RemoveBody bool        `json:"remove_body,omitempty"`
 }
 
 // CreateConfig creates the default plugin configuration.
 func CreateConfig() *Config {
-	return &Config{}
+	return &Config{
+		Overrides:  map[int]int{},
+		RemoveBody: false,
+	}
 }
 
 // ReturnCodeOverride plugin
-type RcOverride struct {
-	next     http.Handler
-	name     string
-    // ...
+type responseCodeOverride struct {
+	next       http.Handler
+	name       string
+	overrrides map[int]int
+	removeBody bool
 }
 
 // New created a new plugin.
 func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
-	// ...
-	return &RcOverride{
-		// ...
+
+	return &responseCodeOverride{
+		overrrides: config.Overrides,
+		next:       next,
+		removeBody: config.RemoveBody,
 	}, nil
 }
 
-func (e *RcOverride) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
-	// ...
-	e.next.ServeHTTP(rw, req)
+// Type we use to signal that nothing else writes and we will be able to change the status
+// code
+type responseCodeOverrideWriter struct {
+	http.ResponseWriter
+
+	buffer     bytes.Buffer
+	overridden bool
+	plugin     *responseCodeOverride //back reference to access the configuration
+}
+
+func (w *responseCodeOverrideWriter) WriteHeader(responseCode int) {
+
+	if val, ok := w.plugin.overrrides[responseCode]; ok {
+		w.overridden = true
+		w.ResponseWriter.WriteHeader(val)
+		return
+	}
+
+	w.ResponseWriter.WriteHeader(responseCode)
+	return
+}
+
+func (w *responseCodeOverrideWriter) Write(b []byte) (int, error) {
+	if !w.overridden {
+
+		// If not overriden yet and we do have a config for such
+		// then we need to override that
+		if val, ok := w.plugin.overrrides[200]; ok {
+			w.overridden = true
+			w.ResponseWriter.WriteHeader(val)
+		}
+	}
+
+	return w.buffer.Write(b)
+}
+
+func (e *responseCodeOverride) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+
+	respCodeOverrideWriter := &responseCodeOverrideWriter{ResponseWriter: rw, plugin: e}
+	e.next.ServeHTTP(respCodeOverrideWriter, req)
+
+	bodyBytes := respCodeOverrideWriter.buffer.Bytes()
+
+	if e.removeBody {
+		rw.Write(make([]byte, 0))
+	} else {
+		rw.Write(bodyBytes)
+	}
+
 }
